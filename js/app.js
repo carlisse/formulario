8470dc191b9e1d85961fe2652be2a1504d672f8b
elizabeth_sanchez <eliz9016@hotmail.es>
2023-07-05 12:17:01 -0700

    menu.js formulario.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,29 @@
 const hamburguesa = document.querySelector('.hamburguesa');
 const navegacion = document.querySelector('.navegacion');
-const enlaces = document.querySelector('.navegacion a');
+const enlaces = document.querySelectorAll('.navegacion a');
+
+document.addEventListener('DOMContentLoaded',()=>{
+    mostrarMenu();
+    cerrarMenu();
+});
+
+function mostrarMenu(){
+    hamburguesa.addEventListener('click',()=>{
+        navegacion.classList.toggle('ocultar');
+
+    });
+
+}
+function cerrarMenu(){
+    enlaces.forEach(enlace => {
+        enlace.addEventListener('click',(e)=>{
+            
+            if(e.target.tagName === 'A'){
+                navegacion.classList.add('ocultar');
+            };
+        });
+    });
+}
+
+//formulario//
+
