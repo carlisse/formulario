c17a6d52b052ffb675e42f473f138e828063aa61
elizabeth_sanchez <eliz9016@hotmail.es>
2023-07-05 21:50:18 -0700

    arreglos error formulario

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const hamburguesa = document.querySelector('.hamburguesa');
+/*const hamburguesa = document.querySelector('.hamburguesa');
 const navegacion = document.querySelector('.navegacion');
 const enlaces = document.querySelectorAll('.navegacion a');
 
@@ -23,7 +23,32 @@ function cerrarMenu(){
             };
         });
     });
-}
+}*/
 
 //formulario//
 
+const usuario = document.getElementById("usuario")
+const password = document.getElementById("password")
+const form = document.getElementById("form")
+const parrafo = document.getElementById("warnings")
+
+form.addEventListener("submit", e=>{
+    e.preventDefault()
+    let warnings = ""
+    let entrar = false
+    if(usuario.value.length <6){
+        warnings += `El nombre no es valido <br>`
+        entrar = true
+    }
+    if(password.value.length < 8){
+        warnings += `La contraseña no es valida <br>`
+        entrar = true
+    }
+    if(entrar){
+        parrafo.innerHTML = warnings
+    }else{
+        parrafo.innerHTML = "Enviado"
+    }
+
+})
+    
